9f6f48e165a5d0ebb074dde93c63fff95e28947f
Priyank Thakkar <priyank@liven.com.au>
2022-03-08 15:37:56 +1100

    Push changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMachine } from "@xstate/react";
+import { createMachine, assign } from "xstate";
+import "./App.css";
+
+const authenticateUserNew = async (c, e) => {
+  console.log("service invoked");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.5) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 1000);
+  });
+};
+
+const loginMachine = createMachine(
+  {
+    id: "login-machine",
+    initial: "unauthenticated",
+    context: {
+      isAuthenticated: false,
+    },
+    states: {
+      unauthenticated: {
+        on: {
+          AUTHENTICATING: {
+            invoke: {
+              id: "authenticateUser",
+              src: (c, e) => authenticateUserNew(c, e),
+              onDone: {
+                target: "authenticated",
+                actions: assign({ isAuthenticated: (context, event) => true }),
+              },
+              onError: {},
+            },
+          },
+        },
+      },
+      authenticated: {
+        on: {
+          LOGOUT: {
+            target: "unauthenticated",
+          },
+        },
+      },
+    },
+  },
+  {
+    services: {
+      authenticateUser: () => {
+        console.log("service invoked");
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (Math.random() > 0.5) {
+              resolve();
+            } else {
+              reject();
+            }
+          }, 1000);
+        });
+      },
+    },
+  }
+);
 
 function App() {
+  const [current, send] = useMachine(loginMachine);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{current.value}</h2>
+      <br />
+      <h3>
+        isAuthenticated: {current.context.isAuthenticated ? "True" : "False"}
+      </h3>
+      <br />
+      <button onClick={() => send("AUTHENTICATING")}>AUTHENTICATE</button>
+      <br />
+      <button onClick={() => send("LOGOUT")}>LOGOUT</button>
     </div>
   );
 }
